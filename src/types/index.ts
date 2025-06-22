48c74eb7b5bd5e3b51f2ba2e92c89c664e294fd1
andria.gv <gvaramiaandria1@gmail.com>
2025-06-22 13:58:27 +0400

    Write Project model

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export interface Project {
+  id: number;
+  title: string;
+  category: string;
+  subcategory: string;
+  description: string;
+  imageUrl: string;
+  tags?: string[];
+  githubUrl?: string;
+  gitlabUrl?: string;
+  liveUrl?: string;
+  certificateUrl?: string;
+  facebookUrl?: string;
+  linkedinUrl?: string;
+} 
